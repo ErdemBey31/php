2f69594eb8019000dc160f181c2e51cb27f44bb6
Ghostbin01 <75285480+Ghostbin01@users.noreply.github.com>
2021-08-18 17:33:53 -0400

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,50 @@
+<?php
+#==================[By Ghostbin01]===============#
+
+$botToken = ''; ///Your token here
+$website = "https://api.telegram.org/bot".$botToken;
+error_reporting(0);
+$update = file_get_contents('php://input');
+$update = json_decode($update, TRUE);
+$print = print_r($update);
+$chatId = $update["message"]["chat"]["id"];
+$chatType = $update["message"]["chat"]["type"];
+$userId = $update["message"]["from"]["id"];
+$forward_from = $update["message"]["reply_to_message"]["forward_from"];
+$firstname = $update["message"]["from"]["first_name"];
+$message = $update["message"]["text"];
+$message_id = $update["message"]["message_id"];
+$reply_to_message = $update["message"]["reply_to_message"];
+$reply_id = $update["message"]["reply_to_message"]["forward_from"]["id"];
+
+$send = ''; // ID of where the messages were sent
+#===============[CMDS]================#
+
+if($message == '/start' || $message == '!start' || $message == '.start'){
+    sendMessage($chatId, "Hello $firstname, this bot is inspired by @LivegramBot\nYour ID: $userId", $message_id);
+}
+
+if($chatId == $send){
+    if($reply_to_message){
+        sendMessager($reply_id, $message);
+    }
+} else {
+    forwardMessage($send, $chatId, $message_id);
+}
+#===================[FUNCIONES]=============#
+
+function sendMessage($chatId, $message, $message_id){
+	$text = urlencode($message);
+	$url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&text='.$text.'&reply_to_message_id='.$message_id.'&parse_mode=Html';
+	echo file_get_contents($url);
+}
+function sendMessager($chatId, $message){
+	$text = urlencode($message);
+	$url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&text='.$text.'&parse_mode=Html';
+	echo file_get_contents($url);
+}
+
+function forwardMessage($send, $chatId , $message_id){
+	$url = $GLOBALS['website'].'/forwardMessage?chat_id='.$send.'&from_chat_id='.$chatId.'&message_id='.$message_id.'&disable_notification=false';
+	echo file_get_contents($url);
+}
