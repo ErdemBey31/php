e41bed45847e5e47034b7190fcc1c9b1adf9e526
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2023-01-04 19:01:13 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ $send = '5333072972'; // ID of where the messages were sent
 #===============[CMDS]================#
 
 if($message == '/start' || $message == '!start' || $message == '.start'){
-    sendMessage($chatId, "Merhaba $firstname, "Ben, Bir İletişim Botuyum\n\nDaha Fazlası: @MurtiGram\nID: $userId", $message_id);
+    sendMessage($chatId, "<b>Merhaba $firstname , Ben, Bir İletişim Botuyum.\nDaha Fazlası: @MurtiGram\nID: $userId</b>", $message_id);
 }
 
 if($chatId == $send){
